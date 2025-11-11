967d66c41fd88d3ed4ac5fbe381f448dd0256c98
Naseer Shaik <r.naseershaik@temenos.com>
2025-11-11 16:13:42 +0530

    added login

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import db from './database.js';
+import db from './database.js'; 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5050;
-
 
 console.log('Connected to SQLite database.');
 
@@ -27,6 +26,22 @@ app.get('/prime-deals', (req, res) => {
   const primeDeals = db.prepare('SELECT * FROM prime_deals').all();
   res.json(primeDeals);
 });
+
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  
+  const VALID_USERNAME = 'naseer';
+  const VALID_PASSWORD = 'password'; 
+
+  if (username === VALID_USERNAME && password === VALID_PASSWORD) {
+    res.json({ jwt_token: 'dummy_jwt_token_123456' });
+  } else {
+    res.status(401).json({ error_msg: 'Invalid username or password' });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
